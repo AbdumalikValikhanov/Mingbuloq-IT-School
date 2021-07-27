2f2a61cc81b36effde1212f57d9686f17f200067
Lenovo-PC <valikhanovabdumalik@gmail.com>
2021-07-27 14:30:26 +0500

    1-sinov

--- app/src/main/java/com/valikhanovabdumalik/mingbuloqitschool/UI/LoadingFragment.java
+++ app/src/main/java/com/valikhanovabdumalik/mingbuloqitschool/UI/LoadingFragment.java
@@ -1,11 +1,26 @@
 package com.valikhanovabdumalik.mingbuloqitschool.UI;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.valikhanovabdumalik.mingbuloqitschool.R;
 
 public class LoadingFragment extends Fragment {
+    ImageView loading_gif;
     public LoadingFragment() {
         super(R.layout.loading_fragment);
+    }
+
+    @Override
+    public void onViewCreated( View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        loading_gif=view.findViewById(R.id.loading_gif);
+        Glide.with(getContext()).load(R.drawable.loading).into(loading_gif);
     }
 }
