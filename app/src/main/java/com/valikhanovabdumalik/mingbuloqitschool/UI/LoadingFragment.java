72eb93f89f9de582ed4b9de04c32c5c0ee37e920
Lenovo-PC <valikhanovabdumalik@gmail.com>
2021-07-27 14:01:16 +0500

    1-sinov

---
+++ app/src/main/java/com/valikhanovabdumalik/mingbuloqitschool/UI/LoadingFragment.java
@@ -0,0 +1,11 @@
+package com.valikhanovabdumalik.mingbuloqitschool.UI;
+
+import androidx.fragment.app.Fragment;
+
+import com.valikhanovabdumalik.mingbuloqitschool.R;
+
+public class LoadingFragment extends Fragment {
+    public LoadingFragment() {
+        super(R.layout.loading_fragment);
+    }
+}
---
+++ app/src/main/java/com/valikhanovabdumalik/mingbuloqitschool/UI/MainFragment.java
@@ -0,0 +1,11 @@
+package com.valikhanovabdumalik.mingbuloqitschool.UI;
+
+import androidx.fragment.app.Fragment;
+
+import com.valikhanovabdumalik.mingbuloqitschool.R;
+
+public class MainFragment extends Fragment {
+    public MainFragment() {
+        super(R.layout.main_fragment);
+    }
+}
